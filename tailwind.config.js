2507ca8562342dddd833f930b4b7d0a720d13311
AashishRauniyar <rauniyaaraashish@gmail.com>
2024-04-01 12:28:58 +0545

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+	  './src/main/webapp/**/*.jsp',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+	  require('@tailwindcss/aspect-ratio'),
+  ],
+}
+
